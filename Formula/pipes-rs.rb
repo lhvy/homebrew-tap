e387d6b0fb6d64553727875007331f1c682cbd9d
CookieCoder15 <40507205+CookieCoder15@users.noreply.github.com>
2021-02-05 15:05:15 +1100

    Fix hash

--- Formula/pipes-rs.rb
+++ Formula/pipes-rs.rb
@@ -2,7 +2,7 @@ class PipesRs < Formula
   desc "An overengineered rewrite of pipes.sh in Rust"
   homepage "https://github.com/CookieCoder15/pipes-rs"
   url "https://github.com/CookieCoder15/pipes-rs/releases/download/v1.0.0/pipes-rs.tar.gz"
-  sha256 "c9879dcee5f0b17fe6d2df0a1e3a0942cf0ed885bc858bbba60e6571df83bb8b"
+  sha256 "348de92c322c57180de308e208ddcb2360b16b43d5d36218d4f3c1cc2538a91d"
   version "1.0.0"
 
   def install
