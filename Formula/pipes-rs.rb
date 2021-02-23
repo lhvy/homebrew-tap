1319fda762e78ee3f02fcdcf445689be0021401e
Lucas <40507205+CookieCoder15@users.noreply.github.com>
2021-02-23 17:39:18 +1100

    pipes-rs 1.0.3
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/pipes-rs.rb
+++ Formula/pipes-rs.rb
@@ -1,9 +1,9 @@
 class PipesRs < Formula
   desc "An overengineered rewrite of pipes.sh in Rust"
   homepage "https://github.com/CookieCoder15/pipes-rs"
-  url "https://github.com/CookieCoder15/pipes-rs/releases/download/v1.0.2/pipes-rs.tar.gz"
-  sha256 "c89e9f5f1218cfc9ae94fd37b129adad9b7e1e9dbbb9c3186b1bc50b1b4fa83d"
-  version "1.0.2"
+  url "https://github.com/CookieCoder15/pipes-rs/releases/download/v1.0.3/pipes-rs.tar.gz"
+  sha256 "96999ebd1e856d3b7066799a95af08cb043eeb58160e8fece552a86d637b5b03"
+  version "1.0.3"
 
   def install
     bin.install "release/pipes-rs"
