58179ce84f869edc11a24b518895bc28e7420428
Lucas <cookiecoder15@gmail.com>
2021-03-28 12:31:17 +1100

    Remove old username

--- Formula/pipes-rs.rb
+++ Formula/pipes-rs.rb
@@ -1,7 +1,7 @@
 class PipesRs < Formula
   desc "An over-engineered rewrite of pipes.sh in Rust"
-  homepage "https://github.com/CookieCoder15/pipes-rs"
-  url "https://github.com/CookieCoder15/pipes-rs/releases/download/v1.4.1/pipes-rs-mac-x86_64.tar.gz"
+  homepage "https://github.com/lhvy/pipes-rs"
+  url "https://github.com/lhvy/pipes-rs/releases/download/v1.4.1/pipes-rs-mac-x86_64.tar.gz"
   sha256 "f985b3e545ff64c9eb767be467e51ef000ba993f04feeff24ede717131e2798b"
   version "1.4.1"
 
