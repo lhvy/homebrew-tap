bf708cdf6ef3c7369661e20722f5e1ad1c59b5d7
Lucas <me@lhvy.dev>
2021-06-18 14:59:02 +1000

    pipes-rs 1.4.4
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/pipes-rs.rb
+++ Formula/pipes-rs.rb
@@ -1,9 +1,9 @@
 class PipesRs < Formula
   desc "An over-engineered rewrite of pipes.sh in Rust"
   homepage "https://github.com/lhvy/pipes-rs"
-  url "https://github.com/lhvy/pipes-rs/releases/download/v1.4.3/pipes-rs-mac-x86_64.tar.gz"
-  sha256 "0c7d5f7d94d2b92b00327ff3d427138234bb0b6a35f771311dafaa70a038b4fd"
-  version "1.4.3"
+  url "https://github.com/lhvy/pipes-rs/releases/download/v1.4.4/pipes-rs-mac-x86_64.tar.gz"
+  sha256 "0586891507f0985afafe7652c74385c2c274b367bcc3f9119e34f9afcce6eee2"
+  version "1.4.4"
 
   def install
     bin.install "release/pipes-rs"
